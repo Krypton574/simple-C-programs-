94fa725296ff93a7f15dc03d903b2dcfaa3fe4f4
Krypton_777 <109064974+Krypton574@users.noreply.github.com>
2022-07-11 14:16:08 +0530

    Add files via upload
     a simple voting eligibility checking program

---
+++ c program to check wheather a person is eligible to vote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+                 int age ;
+
+                 print("Enter age : ")
+                 scanf("%d" , &age) ;
+
+                 if ( age   >=18 )
+                              printf( " you can vote ! " ) ;
+                else
+                              printf ( " you cant vote ! " ) ;
+
+               return  0 ;
+}
